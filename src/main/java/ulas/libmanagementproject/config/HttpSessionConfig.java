3a4b14bb968bcecf55cb9fee9c3d0b5b0e90d048
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-09-03 10:36:24 +0300

    commit

--- src/main/java/ulas/libmanagementproject/config/HttpSessionConfig.java
+++ src/main/java/ulas/libmanagementproject/config/HttpSessionConfig.java
@@ -3,6 +3,6 @@ package ulas.libmanagementproject.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class HttpSessionConfig {
+    //tamamlanacak. şu an ki hali ile session çalışıyor
 }
